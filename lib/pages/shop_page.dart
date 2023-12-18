c58c21acc7b5c79284bbadcaf27824c134c205a8
IwanPWK <koadhyow@gmail.com>
2023-12-18 07:09:36 +0700

    Semi Final

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import '../components/drink_tile.dart';
+import '../models/drink.dart';
+import '../models/shop.dart';
+import '../pages/order_page.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -8,8 +13,55 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  void goToOrderPage(Drink drink) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OrderPage(
+            drink: drink,
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<BubbleTeaShop>(
+        builder: (context, value, child) => SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  children: [
+                    //heading
+                    const Text(
+                      "Bubble Tea Shop",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.brown,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    //list of drinks for sale
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: value.shop.length,
+                            itemBuilder: (context, index) {
+                              // get individual drink from shop
+                              Drink individualDrink = value.shop[index];
+
+                              // return that drink as a nice tile
+                              return DrinkTile(
+                                drink: individualDrink,
+                                onTap: () => goToOrderPage(individualDrink),
+                                trailings: const Icon(Icons.arrow_forward),
+                              );
+                            }))
+                  ],
+                ),
+              ),
+            ));
   }
 }
